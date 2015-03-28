737d50c2c1d18c5acce0807a9b74d94e31b84475
Aaron Siegel <datadreamerlabs@gmail.com>
2015-03-27 18:08:37 -0700

    API call to grab scaled photo entry.

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 #!/home/asiegel/dev/bin/python
 
-print "Content-Type: text/plain\n"
-
+import sys
 import MySQLdb
 import json
+from PIL import Image
+import StringIO
 import cgi
 import cgitb
 cgitb.enable()
@@ -12,16 +13,38 @@ class SpatialMemory:
 
 	def __init__(self):
 		self.connectToDatabase()
-		self.listLocalPhotos()		
+		self.form = cgi.FieldStorage()
+		action = cgi.escape(self.form["action"].value)
+		if action == "local":
+			self.listLocalPhotos()
+		elif action == "photo":
+			self.getPhoto()
 
 	def connectToDatabase(self):
 		self.conn = MySQLdb.connect(host="localhost", user="asiegel_web", passwd="buttslol!", db="asiegel_histmap")
 		self.c = self.conn.cursor()
 
+	def getPhoto(self):
+		# return photo sized to constrain to user's screen dimensions
+		id = cgi.escape(self.form["id"].value)
+		sw = int(cgi.escape(self.form["sw"].value))
+		sh = int(cgi.escape(self.form["sh"].value))
+		self.c.execute("SELECT collection_id,item_id FROM photos WHERE id = "+id)
+		result = self.c.fetchone()
+		#print str(result[0]) +"/"+ str(result[1]) +".jpg"
+		img = Image.open("photos/"+ str(result[0]) +"/"+ str(result[1]) +".jpg")
+		neww = sw
+		newh = (sw / float(img.size[0])) * img.size[1]
+		img = img.resize((int(neww), int(newh)), Image.BICUBIC)
+		output = StringIO.StringIO()
+		img.save(output, "JPEG")
+		sys.stdout.write("Content-Type: image/jpeg\r\n\r\n" + output.getvalue())
+		output.close()
+
 	def listLocalPhotos(self):
-		form = cgi.FieldStorage()
-		myLat = form["lat"].value
-		myLon = form["lon"].value
+		print "Content-Type: text/plain\n"
+		myLat = cgi.escape(self.form["lat"].value)
+		myLon = cgi.escape(self.form["lon"].value)
 		radius = "0.005"
 		self.c.execute("SELECT *, (SELECT sqrt(pow(lat - "+ myLat +", 2) + pow(lon - "+ myLon +", 2))) AS dist FROM photos WHERE abs(lat - "+ myLat +") < "+ radius +" AND abs(lon - "+ myLon +") < "+ radius +" ORDER BY dist ASC")
 		results = self.c.fetchmany(20)
